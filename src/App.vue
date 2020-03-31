f3175f5c3c278ee20316ab63ccf4dbab4ff4aebf
oborys <borysenko.oleksii@gmail.com>
2020-04-01 00:30:55 +0300

    files

--- src/App.vue
+++ src/App.vue
@@ -1,152 +1,9 @@
-# Simple Vue.js dashboard using JSXAPI WebSockets connection
-
-
-###Requirements
-- [Node](https://nodejs.org/en/download/)
-
-###Installation
-
-```
-git clone https://github.com/CiscoDevNet/sample-ws-jsxapi-vue
-cd sample-ws-jsxapi-vue
-
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-```
-
-Using step-by-step instructions below you will create a simple Vue app. 
-In this App you create and template and Vue js methods that can process data from Webex devices.
-We also create a WebSocket connection and track different types of events.
-
-When you see a box that looks like this:
-
-```
-CLI command /OR/ JS code /OR/ HTML
-```
-
-You can read description of this block and copy this text and paste it into the IDE/terminal. 
-
-Here is a brief cheat sheet:
-For Linux:
-
-**Copy - CTRL+INSERT**
-
-**Paste - SHIFT+INSERT**
-
-For Mac:
-
-**Copy - CMD+C**
-
-**Paste - CMD+V**
-
-For Windows:
-
-**Copy - CTRL+C**
-
-**Paste - CTRL+V**
-
-> **Note** all code is in current directory. You can compare your code and final version. In case of errors also refer to the code located in the current directory. 
-
-##First step
-
-Install [Node](https://nodejs.org/en/download/)
-
-Install Vue-cli. Using Vue-cli you can start the project with some of the official Vue project templates or one of the many of the open-source Vue templates and, of course, you can create your own one and use it anywhere
-
-```
-npm install -g vue-cli
-```
-
-> A simple Vue 2.0 Webpack & `vue-loader` setup for quick prototyping. Note this template is not suitable for production - for that you may want to use the [full webpack template](https://github.com/vuejs-templates/webpack).
-
-```
-vue init webpack-simple vue-spa
-```
-Enter information Project name, description, Author, License, Using sass or Press Enter for choosing default variants. 
-
-```
-cd vue-spa/
-npm install
-```
-
-
-### Vue component
-
-There is the _index.html_ file with a simple HTML markup including only the element with identifier `app` in a body. It will be replaced by a vue-generated DOM. That is the reason why you should not use the tag _body_ as as a root element.
-
-In the src directory we have the main.js file which is the entry point for webpack. The **Vue components** are imported here. Also, here we have a root Vue instance that has two properties for now. The property `el` provides the Vue instance with an existing DOM element to mount on. Another one is a render function which generates DOM from _App.vue_. In general, this is all we need to know about the structure of the _webpack-simple template_, not so much, isn’t it? The main part of our **Vue app** will be coded in _App.vue_. The **.vue** extension indicates that this file is a single-file vue component. It is one of the Vue’s features, let's get to know it better.
-
-
-Install **[jsxapi](https://github.com/cisco-ce/jsxapi)** package.
-It's set of tools to integrate with the Cisco Telepresence Endpoint APIs in JavaScript.
-
-```
-npm install --save-dev jsxapi
-```
-
-Install [Vuetify.js](https://vuetifyjs.com/en/) (Vue Material Design Component Framework)
-
-```
-npm install vuetify
-```
-
-For importing font and icons open _index.html_ and insert in `<head>` `</head>` tag
-
-```
-<link href=’https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel=”stylesheet”>
-<link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-```
-
-Import Vuetify. In src directory open _main.js_ and insert code below after this line `import App from './App.vue'`
-
-```
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-Vue.use(Vuetify)
-```
-
-### Edit configuration and enable using Websocket
-
-Using **RoomKit GUI** you need to make next changes
-Go to _Setup_ -> _Configurations_
-In left menu chose _HttpClient_ settings and change _AllowInsecureHTTPS_ from False on True
-
-![](img/AllowInsecureHTTPS.png)
-
-Then navigate to your Room Kit gui page like https://10.10.20.30 and approve the room kit self-sign certificate in your browser
-![](img/self-sign_certificate.png)
-
-WebSocket's library that we use will indicate an error when we using HTTP
-
-Also, Using RoomKit GUI you need to enable using Websocket
-
-_Go to Setup_ -> _Configurations_
-
-In left menu chose NetworkServices settings and change Websocket from off on FollowHTTPService
-
-![](img/FollowHTTPService.png)
-
-
-### Edit App.vue
-
-In src directory open _App.vue_ file. Inside `<template>` `</template>` `<main>` `</main>` tag insert
-to create form like this
-
-![](img/v_form.png)
-
-We use standart [form sample](https://vuetifyjs.com/en/components/forms) 
-
-<details><summary>CLICK ME</summary>
-<p>
-
-```
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue App</h1>
+    </header>
+    <main>
       <v-form v-model="valid" ref="form">
         <v-container>
           <v-row>
@@ -202,18 +59,10 @@ We use standart [form sample](https://vuetifyjs.com/en/components/forms)
           </v-row>
         </v-container>
       </v-form>
-```
-</p>
-</details>
+      <div class="content">
+      </div>
 
-After this, we create two [cards](https://vuetifyjs.com/en/components/cards) for collecting and displaying information.
-In one of the cards, we insert form for a demo of WebSockets connection.
-We can change the System Unit Name for testing online tracking this type of change. 
 
-<details><summary>CLICK ME</summary>
-<p>
-
-```
       <v-container class="grey lighten-5">
         <v-row no-gutters>
           <v-col
@@ -341,19 +190,19 @@ We can change the System Unit Name for testing online tracking this type of chan
           </v-col>
         </v-row>
       </v-container>
-```
-</p>
-</details>
 
-Inside `<script>` `</script>` tag
+    </main>
+  </div>
+</template>
 
-Import jsxapi module
-```
-const jsxapi = require('jsxapi');
-``` 
-All code snippets below you need insert in `export default {INSERT_CODE}`
-Then paste the block
-```
+<script>
+  import axios from 'axios'
+
+  // import jsxapi module
+  const jsxapi = require('jsxapi');
+
+
+  export default {
     data() {
       return {
         // the block where we initialize the variables for further reassignment in functions
@@ -384,20 +233,16 @@ Then paste the block
         progress: false
       }
     },
-```
-After this block edits the computed property.
-A computed property is where the variable 'fieldIsEmpty' can take on the value of 'true' or 'false',
-depending on whether the fields are filled or unfilled, respectively.
-```    
+    // the computed property where the variable 'fieldIsEmpty' can take on the value of 'true' or 'false',
+    // depending on whether the fields are filled or unfilled, respectively
     computed: {
       fieldIsEmpty: function () {
         return !!(this.login && this.password && this.ip)
       },
     },
-```
-Create methods and functions
+    created() {
 
-```
+    },
     methods: {
       // when users clicked on the 'submit' button function below will be called
       submit() {
@@ -539,18 +384,39 @@ Create methods and functions
         }
       },
     }
-```
+  }
+</script>
 
-Run the App
-```
-npm run dev
-```
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-**Other Useful links**
+  h1, h2 {
+    font-weight: normal;
+  }
 
-- [RoomKit Sandbox](https://devnetsandbox.cisco.com/RM/Diagram/Index/a01c15fc-af6e-497a-92ef-138e06cad308?diagramType=Topology)
-- [Collaboration Endpoint API reference guide](https://www.cisco.com/c/dam/en/us/td/docs/telepresence/endpoint/ce910/collaboration-endpoint-software-api-reference-guide-ce910-temp.pdf)
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-To access some sandbox/services you will need to use Cisco AnyConnect:
-- For Mac and Windows, you can download [here](https://developer.cisco.com/site/sandbox/anyconnect/)
-- For Linux, install OpenConnect, [see here](https://developer.cisco.com/learning/modules/dev-setup/dev-ubuntu/step/7)
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .mx-auto {
+    margin-left: 170px;
+    margin-buttom: 100px;
+  }
+</style>
